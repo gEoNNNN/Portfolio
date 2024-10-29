1920091cf1e324ee52674c9bb697ef6e455d8a93
gEoNNNN <eugen.ostafi@isa.utm.md>
2024-10-29 18:38:58 +0200

    backgraund + static text and menu bar

--- src/app/HomeScreen.tsx
+++ src/app/HomeScreen.tsx
@@ -4,12 +4,17 @@ import Menu from '../components/Menu';
 function HomeScreen() {
   return (
     <div className='body'>
-    <Menu/>
-    <p>I Am</p>
-    <p>Front-End</p>
-    <p>Developer</p>
-    <p>I am a front-end developer, proficient in coding and maintaining 
-    websites to ensure functionality and responsiveness. </p>
+      <Menu />
+      <div className='text'>
+        <p className='text_iam'>I Am</p>
+        <p className='text_fe'>Front-End</p>
+        <p className='text_developer'>Developer</p>
+        <p className='text_description_one'>I am a front-end developer, proficient in coding and maintaining websites to ensure functionality and responsiveness.</p>
+        {/*<p className='text_description_two'>websites to ensure functionality and responsiveness.</p>
+        <a href="/path/to/your/cv.pdf" className='download-button' download>
+          <span>Download CV</span>
+        </a>*/}
+      </div>
     </div>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,17 @@
-
 import './Menu.scss';
 
 function Menu() {
   return (
-    <>
-    <p className='signature'>Eugen Ost.</p>
+    <div className='position'>
+      <p className='signature'>Eugen Ost.</p>
       <ul className='menu'>
         <li>Home</li>
         <li>Projects</li>
         <li>Contact</li>
       </ul>
-    <p className='language'>Ro|En</p>
-    </>
+      <p className='language'>Ro|En</p>
+    </div>
   );
 }
 
-export default Menu;
+export default Menu;  
